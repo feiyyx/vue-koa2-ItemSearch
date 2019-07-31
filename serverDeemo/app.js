b42e8caa85dce4fa9539b7bdf1b5cdb6534328ca
feiyyx <31006315+feiyyx@users.noreply.github.com>
2019-07-31 16:46:40 +0800

    Update app.js
    delete something

--- serverDeemo/app.js
+++ serverDeemo/app.js
@@ -3,7 +3,6 @@ const app = new Koa()
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const cors = require('@koa/cors');
 const koabody = require('koa-body');
